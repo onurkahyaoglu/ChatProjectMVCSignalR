b73e9b2797c4b8eb57a1c036f372e73052abae40
onur kahyaoğlu <onurkahyaoglu@gmail.com>
2022-08-17 12:31:40 +0300

    Chat için hub dosyası ve mesaj akış fonksiyonu oluşturuldu

---
+++ ChatProjectMVCSignalR/Chub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatProjectMVCSignalR
+{
+    public class Chub : Hub
+    {
+        public void Send(string username, string message)
+        {
+            Clients.All.sendMessage(username,message);
+        }
+    }
+}
